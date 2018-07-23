96ca4647eba412ce957fb64e933f2f1c875c3370
samudhanv <vvramakrishna24@gmail.com>
2018-07-23 11:43:21 +0530

    My First Push

---
+++ src/main/java/in/kittu/core/model/Employee.java
@@ -0,0 +1,28 @@
+package in.kittu.core.model;
+
+
+public class Employee {
+	private int empid;
+	private String ename;	
+	private double salary;
+	
+	public int getEmpid() {
+		return empid;
+	}	
+	public void setEmpid(int empid) {
+		this.empid = empid;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	public double getSalary() {
+		return salary;
+	}
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+	
+}
