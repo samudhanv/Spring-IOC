7748da0ba7e5a8b8577086f91216a7a87d03037b
samudhanv <vvramakrishna24@gmail.com>
2018-07-24 10:23:17 +0530

    commit

---
+++ SpringScopeDemo2/src/main/java/in/suresh/TestApp.java
@@ -0,0 +1,24 @@
+package in.suresh;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import in.suresh.model.Address;
+import in.suresh.model.Person;
+
+public class TestApp {
+	
+	public static void main(String[] args) {
+		ApplicationContext container = new ClassPathXmlApplicationContext("springbean.xml");
+		Person p1 = (Person) container.getBean("person");
+		Person p2 = (Person) container.getBean("person");
+		Person p3 = (Person) container.getBean("person");
+		Person p4 = (Person) container.getBean("person");
+	
+		Address a1 = (Address) container.getBean("address");
+		Address a2 = (Address) container.getBean("address");
+		Address a3 = (Address) container.getBean("address");
+		Address a4 = (Address) container.getBean("address");
+	}
+
+}
---
+++ SpringScopeDemo2/src/main/java/in/suresh/model/Person.java
@@ -0,0 +1,33 @@
+package in.suresh.model;
+
+public class Person {
+
+	private int pid;
+	private String pname;
+
+	public Person() {
+		System.out.println("person object created");
+	}
+
+	public int getPid() {
+		return pid;
+	}
+
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+
+	public String getPname() {
+		return pname;
+	}
+
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+
+	@Override
+	public String toString() {
+		return "Person [pid=" + pid + ", pname=" + pname + "]";
+	}
+
+}
